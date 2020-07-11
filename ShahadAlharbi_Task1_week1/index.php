a114915c086bbd6cbe5500b1dc44ebefe5b5816f
Shahad-Saud <67935956+Shahad-Saud@users.noreply.github.com>
2020-07-11 11:22:14 +0300

    Add files via upload

---
+++ ShahadAlharbi_Task1_week1/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Smart method control panel</title>
+	<!-- Mobile Specific Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<!-- Font-->
+	<link rel="stylesheet" type="text/css" href="css/roboto-font.css">
+	<link rel="stylesheet" type="text/css" href="fonts/line-awesome/css/line-awesome.min.css">
+	<!-- Main Style Css -->
+    <link rel="stylesheet" href="css/style.css"/>
+</head>
+<body class="form-v2">
+	<div class="page-content">
+		<div class="form-v2-content">
+			<div class="form-left">
+				<img src="images/form-v2.jpg" alt="form" width="400" height="600">
+	
+				<div class="text-1">
+					<p>Direction  Of SM Robort <span> </span></p>
+
+				</div>
+			</div>
+			<form class="form-detail" action="#" method="post" id="myform">
+			<br><br><br><br><br><br><br>
+				<h2>Control Panel</h2>
+				<div class="form-row">
+					<input type="submit" name="Right"
+				value="Right"  /> 
+				
+					
+				</div>
+				<div class="form-row">
+					
+					<input type="submit" name="Left"
+				value="Left"/> 
+
+				</div>
+				<div class="form-row">
+				
+				<input type="submit" name="Reset"
+				value="Reset"/> 
+				
+				</div>
+				<div class="form-row">
+					
+				<input type="submit" name="Forward"
+				value="Forward"/> 
+				
+				</div>
+				<div class="form-row">
+			
+				<input type="submit" name="Backward"
+				value="Backward"/> 
+				
+				</div>
+			
+				
+			</form>
+		</div>
+	</div>
+</body>
+</html>
